0b26d9abd85ac956e62556d26cda0470d3c12068
chengyin <chengyinok@qq.com>
2018-08-02 15:31:46 +0800

    Add activiti 5.22.0

--- web/src/main/java/com/sun/fighter/SunFighterApplication.java
+++ web/src/main/java/com/sun/fighter/SunFighterApplication.java
@@ -1,17 +1,16 @@
 package com.sun.fighter;
 
-import lombok.extern.slf4j.Slf4j;
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@SpringBootApplication
-@Slf4j
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class SunFighterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SunFighterApplication.class, args);
-		log.info("http://localhost:8080");
+		System.out.printf("http://localhost:8080");
 	}
 }
