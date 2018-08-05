d9589d07009645c8190c25d95730b863dfbfceff
chengyinok <chengyinok@qq.com>
2018-08-05 12:19:25 +0800

    Mod 项目结构变更。

---
+++ sun-web/src/main/java/com/sun/fighter/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.sun.fighter.exception;
+
+import fighter.common.JsonData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @创建人 chengyin
+ * @创建时间 2018/7/25
+ * @描述
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView handler(HttpServletRequest request, Exception e) {
+        String url = request.getRequestURL().toString();
+        JsonData result = JsonData.fail(e.getMessage());
+        log.info("exception,url:" + url, e);
+        ModelAndView mv = new ModelAndView("exception",result.toMap());
+        return mv;
+    }
+}
---
+++ sun-web/src/main/java/com/sun/fighter/shiro/SysUserRealm.java
@@ -0,0 +1,82 @@
+package com.sun.fighter.shiro;
+
+import com.sun.fighter.study.domain.SysUser;
+import com.sun.fighter.study.user.service.SysUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @创建人 chengyin
+ * @创建时间 2018/7/24
+ * @描述
+ */
+@Configuration
+@Slf4j
+public class SysUserRealm extends AuthorizingRealm {
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    /**
+     * 认证回调函数,登录时调用
+     * 首先根据传入的用户名获取User信息；然后如果user为空，那么抛出没找到帐号异常UnknownAccountException；
+     * 如果user找到但锁定了抛出锁定异常LockedAccountException；最后生成AuthenticationInfo信息，
+     * 交给间接父类AuthenticatingRealm使用CredentialsMatcher进行判断密码是否匹配，
+     * 如果不匹配将抛出密码错误异常IncorrectCredentialsException；
+     * 另外如果密码重试此处太多将抛出超出重试次数异常ExcessiveAttemptsException；
+     * 在组装SimpleAuthenticationInfo信息时， 需要传入：身份信息（用户名）、凭据（密文密码）、盐（username+salt），
+     * CredentialsMatcher使用盐加密传入的明文密码和此处的密文密码进行匹配。
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        //获取用户的输入的账号.
+        String username = (String) authenticationToken.getPrincipal();
+        log.info("登录账号：{}", username);
+        SysUser sysUser = sysUserService.findByUserName(username);
+        if (sysUser == null) {
+            throw new UnknownAccountException();
+        }
+        if ("1".equals(sysUser.getStatus())) {
+            throw new LockedAccountException();
+        }
+        // 从数据库查询出来的账号名和密码,与用户输入的账号和密码对比
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                username, //用户名
+                sysUser.getPassword(),//密码
+                ByteSource.Util.bytes(sysUser.getUserName() + sysUser.getSalt()),//salt = 用户名+盐
+                getName()); //realm name
+        Session session = SecurityUtils.getSubject().getSession();
+        session.setAttribute("USER_SESSION", sysUser);
+        return authenticationInfo;
+    }
+
+    /**
+     * 只有需要验证权限时才会调用, 授权查询回调函数, 进行鉴权但缓存中无用户的授权信息时调用.在配有缓存的情况下，只加载一次.
+     * 如果需要动态权限,但是又不想每次去数据库校验,可以存在ehcache中.自行完善
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        log.info("--权限配置--");
+        SysUser sysUser = (SysUser) principals.getPrimaryPrincipal();
+        // 权限信息对象info,用来存放查出的用户的所有的角色（role）及权限（permission）
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+//        for(SysRole role:userInfo.getRoleList()){
+//            authorizationInfo.addRole(role.getRole());
+//            for(SysPermission p:role.getPermissions()){
+//                authorizationInfo.addStringPermission(p.getPermission());
+//            }
+//        }
+        return info;
+    }
+
+
+}
